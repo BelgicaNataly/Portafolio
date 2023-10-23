2ca203dc65ee2a352cc0137ff63935b5fe9bea13
BelgicaNataly <148803724+BelgicaNataly@users.noreply.github.com>
2023-10-23 15:50:35 -0500

    Add files via upload

---
+++ Lenguaje C/2. Calculo promedio/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	float tareas, evaluaciones, examen, promedio;
+	
+	printf("PROMEDIO \n");
+	printf("Ingrese el promedio de sus tareas: ");
+	scanf("%f", &tareas);
+	printf("Ingrese el promedio de sus evaluaciones: ");
+	scanf("%f", &evaluaciones);
+	printf("Ingrese la calificaci%cn de su examen final: ", 162);
+	scanf("%f", &examen);
+	
+	promedio = (tareas * 0.6) + (evaluaciones * 0.1) + (examen * 0.3);
+	
+	printf("El promedio del estudiante es: %.2f", promedio);
+	
+	return 0;
+}
